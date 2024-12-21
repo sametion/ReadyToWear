a379325ac60fcb9d6bdbf26d7559f0eebbf4e669
sametion <sametacikk@gmail.com>
2024-12-22 00:07:07 +0300

    Dto Config
    Success error messages sth

--- business/Concrete/ProductManager.cs
+++ business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -19,9 +20,31 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            _productDal.Add(product);
+            return new Result(true,"product has added");
+        }
+
         public List<Product> GetAll()
         {
                  return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(c=>c.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+               return _productDal.Get(p=>p.ProductId==productId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice<max && p.UnitPrice>min);
         }
 
         public List<ProductDetailDto> GetProductDetails()
